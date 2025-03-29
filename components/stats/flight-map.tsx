1157c2ed045204bb60f3e42bcea497cfa2bc4f39
Michal Nowak <minowak@protonmail.ch>
2025-03-29 11:28:16 +0100

    Geodesics

--- components/stats/flight-map.tsx
+++ components/stats/flight-map.tsx
@@ -1,19 +1,51 @@
 "use client"
 
-import { MapContainer, Polyline, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { LatLngExpression, LatLngTuple } from "leaflet";
+import L from "leaflet"
+import { GeodesicLine } from 'react-leaflet-geodesic';
 
 import "leaflet/dist/leaflet.css";
+import "leaflet.geodesic";
 import { FlightDataService } from "@/lib/services/flight-data-service";
 import { AirportCodesService } from "@/lib/services/airport-codes-service";
+import { useEffect } from "react";
 
 interface MapProps {
   posix: LatLngExpression | LatLngTuple,
   zoom: number,
 }
 
-export default function FlightMap(props: MapProps) {
+const GeodesicComponent = () => {
+  const map = useMap();
   const flightData = FlightDataService.fetch()
+
+  useEffect(() => {
+    if (!map) return;
+
+    for (let flight of flightData.flights) {
+      const origin = AirportCodesService.getByIata(flight.departureAirport)
+      const destination = AirportCodesService.getByIata(flight.arrivalAirport)
+      L.geodesic(
+        [
+          [
+            [+origin.latitude, +origin.longitude],
+            [+destination.latitude, +destination.longitude]],
+        ],
+        {
+          weight: 3,
+          color: "blue",
+          opacity: 0.5,
+          wrap: false
+        }
+      ).addTo(map);
+    }
+  }, [map]);
+
+  return null;
+};
+
+export default function FlightMap(props: MapProps) {
 
   return (
     <MapContainer
@@ -26,20 +58,7 @@ export default function FlightMap(props: MapProps) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {
-        flightData.flights.map((flight, idx) => {
-          const origin = AirportCodesService.getByIata(flight.departureAirport)
-          const destination = AirportCodesService.getByIata(flight.arrivalAirport)
-
-          return <Polyline
-            key={"flight_path_" + idx}
-            positions={[
-              [+origin.latitude, +origin.longitude],
-              [+destination.latitude, +destination.longitude]
-            ]}
-            color={'red'} />
-        })
-      }
+      <GeodesicComponent />
     </MapContainer>
   );
 }
